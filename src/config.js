06d79ccc7f85aae0da01a5d327741855d48664aa
Glen Maddern <glenmaddern@gmail.com>
2015-04-01 22:12:21 +0800

    back to working

--- src/config.js
+++ src/config.js
@@ -9,8 +9,7 @@ System.config({
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js",
-    "post-css": "loaders/post-css.js"
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
