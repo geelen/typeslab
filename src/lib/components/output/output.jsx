046d10803203f14f95b4b02592161a9fe219279a
Glen Maddern <glenmaddern@gmail.com>
2015-04-02 12:57:35 +0800

    this is UNBELIEVABLE

--- src/lib/components/output/output.jsx
+++ src/lib/components/output/output.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Surface from 'react-canvas/lib/Surface'
+import Text from 'react-canvas/lib/Text'
 import './output.scss!post-css'
 
 export default class Output extends React.Component {
   render() {
+    const textStyle = this.getTextStyle()
     return <div className='Output'>
       <h1>Output</h1>
-      <p>{this.props.message}</p>
+      <Surface width={500} height={400} top={0} left={0}>
+        <Text style={textStyle}>{this.props.message}</Text>
+      </Surface>
     </div>
+  }
+
+  getTextStyle() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: 20,
+      lineHeight: 20,
+      fontSize: 12
+    };
   }
 }
