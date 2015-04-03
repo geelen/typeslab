69511456e6622c7e2310f281247fb7a42744ed3c
Glen Maddern <glenmaddern@gmail.com>
2015-04-03 13:38:27 +0800

    added a byline, i quite like typeslab as a name

--- src/lib/components/output/output.jsx
+++ src/lib/components/output/output.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Surface from 'react-canvas/lib/Surface'
+import Layer from 'react-canvas/lib/Layer'
 import Text from 'react-canvas/lib/Text'
 import FontFace from 'react-canvas/lib/FontFace'
 import measureText from 'react-canvas/lib/measureText'
@@ -34,12 +35,15 @@ export default class Output extends React.Component {
   //}
 
   render() {
-    let lines = this.layoutLines(this.props.lines)
+    let lines = this.layoutLines(this.props.lines),
+      text = 'typeslab.com'
     return <div className='Output'>
       <Surface ref="surface" width={this.props.width + this.spacing * 2} height={lines.totalHeight + this.spacing} top={0} left={0}>
+        <Layer style={this.getBorderStyle(lines.totalHeight)}/>
         {lines.sizedLines.map((line) => {
           return <Line line={line}/>
         })}
+        <Text style={this.getByLineStyle(text, lines.totalHeight)}>{text}</Text>
       </Surface>
     </div>
   }
@@ -49,7 +53,7 @@ export default class Output extends React.Component {
       sizedLines = lines.map(line => {
         let text = line, font, lineHeightFactor
         if (text.match(/^!/)) {
-          text = text.replace(/^!/,'')
+          text = text.replace(/^!/, '')
           font = this.altFont
           lineHeightFactor = 1.25
         } else {
@@ -60,11 +64,44 @@ export default class Output extends React.Component {
         let measurements = measureText(text, 9999, font, 12, 15),
           factor = this.props.width / measurements.width,
           fontSize = Math.min(300, 12 * factor),
-          style = {fontSize, height: fontSize, lineHeight: fontSize, top: totalHeight, width: 500 + 2*this.spacing, fontFace: font, left: 0, textAlign: 'center'}
+          style = {
+            fontSize,
+            height: fontSize,
+            lineHeight: fontSize,
+            top: totalHeight,
+            width: 500 + 2 * this.spacing,
+            fontFace: font,
+            left: 0,
+            textAlign: 'center'}
         totalHeight += fontSize * lineHeightFactor
-        return {line:text, style}
+        return {line: text, style}
       })
     return {totalHeight, sizedLines}
+  }
+
+  getBorderStyle(height) {
+    return {
+      borderColor: '#444',
+      top: this.spacing / 2,
+      width: this.props.width + this.spacing,
+      left: this.spacing / 2,
+      height: height
+    }
+  }
+
+  getByLineStyle(text, height) {
+    let font = FontFace('Avenir Next Condensed, Helvetica, sans-serif', null, {weight: 400}),
+      width = measureText(text, 9999, font, 8, 15).width
+    return {
+      fontFace: font,
+      fontSize: 8,
+      backgroundColor: 'white',
+      textAlign: 'center',
+      width: width + 4,
+      left: (this.props.width + this.spacing) / 2,
+      top: height + 11,
+      height: 16
+    }
   }
 }
 
