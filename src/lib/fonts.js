58cbbd533b9b5be05bf1c983979948442ebd0825
Glen Maddern <glenmaddern@gmail.com>
2015-04-08 09:53:34 +0800

    got a couple of reasonable font pairings to be going on with

--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -1,3 +1,38 @@
-export default {
-
-}
+export default [
+  {
+    main: {
+      local: ['Avenir Next', 'Arial Black'],
+      weight: 900,
+      caps: true
+    },
+    alt: {
+      local: ['Georgia', 'Times New Roman'],
+      weight: 400,
+      italic: true
+    }
+  },
+  {
+    main: {
+      google: 'Open Sans',
+      weight: 800,
+      caps: true
+    },
+    alt: {
+      google: 'Gentium Book Basic',
+      weight: 400,
+      italic: true
+    }
+  },
+  {
+    main: {
+      google: 'Gentium Book Basic',
+      weight: 700,
+      italic: true
+    },
+    alt: {
+      google: 'Open Sans',
+      weight: 100,
+      caps: true
+    }
+  }
+]
