a07fe7dd1f8e6b7178b48e78fde7f49df3473bd3
Glen Maddern <glenmaddern@gmail.com>
2015-04-09 20:10:59 +0800

    aww yis

--- src/lib/components/frame.jsx
+++ src/lib/components/frame.jsx
@@ -8,7 +8,11 @@ import Output from './output.jsx!'
 export default class Frame extends React.Component {
   render() {
     return <div className="Frame">
-      <header><h1 className="Frame-Logo"><span>Type</span><span>Slab</span></h1></header>
+      <header>
+        <h1 className="Frame-Logo">
+          <span>Type</span><span>Slab</span>
+        </h1>
+      </header>
       <Controls></Controls>
       <FluxComponent connectToStores='messages'>
         <Output width={500}></Output>
